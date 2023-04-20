7b23710c54bb4563ee2ef7903ed4a10931010cff
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-04-20 14:56:14 -0400

    Fix workflows and filter homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCountries, selectAllCountries } from '../redux/home/homeSlice';
 
@@ -6,19 +6,45 @@ function Home() {
   const dispatch = useDispatch();
   const countries = useSelector(selectAllCountries);
 
+  const [regionFilter, setRegionFilter] = useState('');
+
   useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
 
+  const handleRegionFilterChange = (event) => {
+    setRegionFilter(event.target.value);
+  };
+
+  const filteredCountries = countries
+    .filter(
+      (country) => country.region.toLowerCase().includes(regionFilter.toLowerCase()),
+    )
+    .map((country) => (
+      <div key={country.cca3}>
+        {country.name.common}
+        {' '}
+        -
+        {country.region}
+      </div>
+    ));
+
   return (
     <div>
       <h1>List of Countries</h1>
-      {countries.map((country) => (
-        <div key={country.cca3}>
-          {country.name.common}
-          {' '}
-        </div>
-      ))}
+      <div>
+        Filter by region:
+        {' '}
+        <select value={regionFilter} onChange={handleRegionFilterChange}>
+          <option value="">All countries</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      {filteredCountries}
     </div>
   );
 }
